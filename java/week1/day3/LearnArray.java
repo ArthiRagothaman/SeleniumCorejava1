a08b7d052411413261b188c0e7d8187949401bd6
Arthi Ragothaman <arthi.mca11@gmail.com>
2024-08-25 16:11:35 +0200

    Update LearnArray.java
    Updated some code

--- java/week1/day3/LearnArray.java
+++ java/week1/day3/LearnArray.java
@@ -11,7 +11,30 @@ public class LearnArray {
 		 System.out.println(length);
 		 //find last value in the array
 		 System.out.println("Last value of the array:" +mark[length-1]);
+
+		//find first index value 
+		System.out.println("print first index value :"+mark[0]);
+		
+		//find the largest number in the array
+		//sort()
+		Arrays.sort(mark);
+		//32,34,35,56,67
+		//0 to 4 -
+		System.out.println("print largest value :"+mark[length-1]);
+		
+		//print all value in console
+		for (int i = 0; i < mark.length; i++) {
+			System.out.println(mark[i]);
+			
+		}
+		
+		for (int i = mark.length-1; i >=0 ; i--) {
+			System.out.println(mark[i]);
+			
+			//ArrayIndexOutOfBoundsException- where you have added length-1
+			//where index 5 is present or not 
 		 
 	}
 
+}
 }
