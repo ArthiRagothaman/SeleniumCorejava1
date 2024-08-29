80c2742a68c9436bd5c10d7e53c0e94e35d3b5c8
Arthi Ragothaman <arthi.mca11@gmail.com>
2024-08-29 20:36:39 +0200

    Update LearnSet.java
    Added lines in this code

--- java/week3/day2/LearnSet.java
+++ java/week3/day2/LearnSet.java
@@ -5,17 +5,51 @@ import java.util.HashSet;
 public class LearnSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] mentors = {"aaa","bbbb","ccccc","dddd","cccc","eeee"};
-		//Set<String>  lst = new HashSet<String>();
-		HashSet<String> res = new HashSet<String>();
-		//for(String s: mentors) {
-			//lst.add(s);
-		//}
-		
-		
-		
+	String[] mentors= {"Vinoth","Vineeth","Bhuvanesh","Vineeth","Sakthi","Bhuvanesh"};
+
+		Set<String> unq=new TreeSet<String>();
+
+		for (String s : mentors)
+			{
+			
+			unq.add(s);
+			}
+
+		System.out.println(unq);
+
+		//set - we not have sort method 
+		//we can use TreeSet
+
+		//convert set into list
+		List<String> val=new ArrayList<String>(unq);
+		String string = val.get(2);
+
+		System.out.println(string);
+
+		// remove duplicate char from the string
+
+		String name="testleaf";
+
+		//convert into toCharArray
+		char[] ch = name.toCharArray();
+
+		//empty set
+		Set<Character> unq1=new LinkedHashSet<Character>();
+
+		for (Character c : ch) {
+			unq1.add(c);
+		}
+
+		System.out.println(unq1);
+
+		//set value and iteration into character
+
+		for (Character character : unq1) {
+			System.out.print(character);
+		}	
+
+	}				
 
 	}
 
-}
+
